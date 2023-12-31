4458777073227f71ebeb5b032c231cdd29b7ed3f
Yiğit TİLKİ <bestnemesis1234@gmail.com>
2023-12-31 15:52:05 +0300

    gitignore

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,7 +2,7 @@
 // ignore_for_file: lines_longer_than_80_chars, avoid_classes_with_only_static_members
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+    show TargetPlatform, defaultTargetPlatform, kIsWeb;
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -78,3 +78,5 @@ class DefaultFirebaseOptions {
     iosBundleId: 'com.yigittilki.flutternewsapp.flutterNewsApp.RunnerTests',
   );
 }
+
+//
