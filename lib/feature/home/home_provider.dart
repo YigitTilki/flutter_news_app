187d2f447226e9ceb6e3d77fd699939b05a064ee
Yiğit <yigittilkiw@gmail.com>
2024-01-19 21:02:06 +0300

    a

---
+++ lib/feature/home/home_provider.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeNotifier extends StateNotifier<HomeState> {
+  HomeNotifier() : super(HomeState());
+}
+
+class HomeState extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
